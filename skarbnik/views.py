cbf2ae63ddff2df5e06e4b023cdf07785b1967c9
Daniel Jurczak <daniel@xdsl-565.wroclaw.dialog.net.pl>
2018-11-24 22:46:59 +0100

    Conflicts

--- skarbnik/views.py
+++ skarbnik/views.py
@@ -24,41 +24,9 @@ class StudentViewset(viewsets.ModelViewSet):
 class UserViewset(viewsets.ModelViewSet):
     queryset = models.User.objects.all()
     serializer_class = serializers.UserSerializer
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-class ChangePasswordView(generics.UpdateAPIView):
-    """
-    An endpoint for changing password.
-    """
-    serializer_class = serializers.ChangePasswordSerializer
-    model = models.User
-    
-
-    def get_object(self, queryset=None):
-        obj = self.request.user
-        return obj
-
-    def update(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        serializer = self.get_serializer(data=request.data)
-
-        if serializer.is_valid():
-            # Check old password
-            if not self.object.check_password(serializer.data.get("old_password")):
-                return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
-            # set_password also hashes the password that the user will get
-            self.object.set_password(serializer.data.get("new_password"))
-            self.object.save()
-            return Response("Success.", status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-=======
-=======
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ('role',)
 
->>>>>>> changepassword
     def get_object(self):
         pk = self.kwargs.get('pk')
 
@@ -66,6 +34,7 @@ class ChangePasswordView(generics.UpdateAPIView):
             return self.request.user
 
         return super(UserViewset, self).get_object()
+    
 
 class UpdatePassword(views.APIView):
     """
@@ -92,19 +61,3 @@ class UpdatePassword(views.APIView):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-<<<<<<< HEAD
-class TeachersViewset(viewsets.ReadOnlyModelViewSet):
-    queryset = models.User.objects.filter(role=1)
-    serializer_class = serializers.TeachersSerializer
->>>>>>> changepassword
-=======
-# class TeachersList(generics.ListAPIView):
-#     serializer_class = TeachersList
-
-#     def get_queryset(self):
-#         """
-#         This view should return a list of all the purchases
-#         for the currently authenticated user.
-#         """
-#         return models.User.objects.filter(role=1)
->>>>>>> changepassword
