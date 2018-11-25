644c4f29d94e73442f128ff49f23a47ce2941bae
Daniel Jurczak <daniel@MacBook-Pro-Daniel.local>
2018-11-25 08:04:01 +0100

    Third revert

--- Hackyeah/urls.py
+++ Hackyeah/urls.py
@@ -26,8 +26,12 @@ router.register(r'student', myapp_views.StudentViewset)
 router.register(r'users', myapp_views.UserViewset)
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 router.register(r'teachers', myapp_views.TeachersViewset, basename='users')
 router.register(r'progress', myapp_views.ProgressViewset, basename='student')
+=======
+
+>>>>>>> parent of e705b6c... Add teachers endpoint
 =======
 
 >>>>>>> parent of e705b6c... Add teachers endpoint
---
+++ skarbnik/views.py
@@ -0,0 +1,68 @@
+
+from rest_framework import viewsets, generics, views, status
+from . import models
+from . import serializers
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+
+class ClassViewset(viewsets.ModelViewSet):
+    queryset = models.Class.objects.all()
+    serializer_class = serializers.ClassSerializer
+
+class PaymentViewset(viewsets.ModelViewSet):
+    queryset = models.Payment.objects.all()
+    serializer_class = serializers.PaymentSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('class_field', )
+class PaymentDetailViewset(viewsets.ModelViewSet):
+    queryset = models.PaymentDetail.objects.all()
+    serializer_class = serializers.PaymentDetailSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('student', )
+
+class StudentViewset(viewsets.ModelViewSet):
+    queryset = models.Student.objects.all()
+    serializer_class = serializers.StudentSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('user',)
+
+class UserViewset(viewsets.ModelViewSet):
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('role',)
+
+    def get_object(self):
+        pk = self.kwargs.get('pk')
+
+        if pk == "current":
+            return self.request.user
+
+        return super(UserViewset, self).get_object()
+    
+
+class UpdatePassword(views.APIView):
+    """
+    An endpoint for changing password.
+    """
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def put(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = serializers.ChangePasswordSerializer(data=request.data)
+
+        if serializer.is_valid():
+            # Check old password
+            old_password = serializer.data.get("old_password")
+            if not self.object.check_password(old_password):
+                return Response({"old_password": ["Wrong password."]}, 
+                                status=status.HTTP_400_BAD_REQUEST)
+            # set_password also hashes the password that the user will get
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
