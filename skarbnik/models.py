ee844dd0fa7d0ac8c76c3d1cfea81052eb20c96c
Daniel Jurczak <danieljur4@gmail.com>
2018-11-29 18:34:16 +0100

    Add functions for populating database

--- skarbnik/models.py
+++ skarbnik/models.py
@@ -22,7 +22,7 @@ class Class(models.Model):
 class Payment(models.Model):
     id_field = models.AutoField(db_column='id_', primary_key=True)  # Field renamed because it ended with '_'.
     class_field = models.ForeignKey(Class, on_delete=models.CASCADE, db_column='class_id')  # Field renamed because it was a Python reserved word.
-    creation_date = models.DateTimeField()
+    creation_date = models.DateTimeField(auto_now_add=True)
     start_date = models.DateField()
     end_date = models.DateField()
     amount = models.DecimalField(max_digits=6, decimal_places=2)
@@ -87,3 +87,51 @@ class UserLoginActivity(models.Model):
     class Meta:
         verbose_name = 'user_login_activity'
         verbose_name_plural = 'user_login_activities'
+
+
+
+import random
+import names
+from datetime import timedelta, datetime
+def random_date(start, end):
+    """
+    This function will return a random datetime between two datetime 
+    objects.
+    """
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = random.randrange(int_delta)
+    return start + timedelta(seconds=random_second)
+
+def create_student():
+    class_field = Class.objects.get(id_field=random.randint(4, 11))
+    user = User.objects.get(id_field=random.randint(76, 122))
+    name = names.get_full_name()
+    c = Student.objects.create(name=name, class_field=class_field, user=user)
+    c.save()
+    return c
+
+def create_payment():
+    d1 = datetime.strptime('12/1/2018', '%m/%d/%Y')
+    d2 = datetime.strptime('1/1/2019', '%m/%d/%Y')
+    d3 = datetime.strptime('1/2/2019', '%m/%d/%Y')
+    d4= datetime.strptime('2/1/2019', '%m/%d/%Y')
+
+    class_field = Class.objects.get(id_field=random.randint(4, 11))
+    start_date = random_date(d1, d2)
+    end_date = random_date(d3, d4)
+    amount = random.randint(10, 1000)
+    name = "Sample tittle"
+    description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et gravida ante. Etiam laoreet est vitae quam volutpat euismod. Nullam congue libero consequat, tempor arcu non, pulvinar elit."
+    c = Payment.objects.create(name=name, class_field=class_field, start_date=start_date, end_date=end_date, amount=amount,description=description)
+    c.save()
+    return c
+def create_detail_payment():
+
+    payment = Payment.objects.get(id_field=random.randint(11, 36))
+    student = random.choice(Student.objects.filter(class_field=payment.class_field))
+    amount_paid = payment.amount - random.randint(0, int(payment.amount))
+
+    c = PaymentDetail.objects.create(payment=payment, student=student,amount_paid=amount_paid)
+    c.save()
+    return c
