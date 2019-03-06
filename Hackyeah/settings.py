ed1809f70a8c5a91af4892c98aaaa523b1174f65
Daniel Jurczak <danieljur4@gmail.com>
2019-03-06 16:45:17 +0100

    Change db to local

--- Hackyeah/settings.py
+++ Hackyeah/settings.py
@@ -99,9 +99,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'Skarbnik3',
-        'USER': 'docker1',
-        'PASSWORD': 'docker1',
-        'HOST': 'db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
             'sql_mode': 'traditional',
